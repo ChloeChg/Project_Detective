ede77c8167c91efcef29fe4311281ef95d0c3a88
gamesfoundation <gamesfoundationfun@gmail.com>
2025-01-18 17:26:10 -0800

    test branch

--- Project_Detective/Assets/Assets/Script/OldClock.cs
+++ Project_Detective/Assets/Assets/Script/OldClock.cs
@@ -5,25 +5,16 @@ using UnityEngine;
 
 public class OldClock : MonoBehaviour, InteractableObject
 {
-    public Dialogue dialogue; // Reference to the Dialogue system
-    public DialogueData dialogueData; // Unique dialogue data for this NPC
-
     public GameObject polaroid;
-    public GameObject inventoryMenu;
+    public GameObject logicPolaroid;
+    public GameObject polaroidInventory;
+    public GameObject logicInventory;
     public void Interact()
     {
-        // Ensure DialogueData is set and Dialogue system is available
-        if (dialogue != null && dialogueData != null)
-        {
-            dialogue.dialogueData = dialogueData; // Assign this NPC's dialogue data
-            dialogue.gameObject.SetActive(true); // Activate the dialogue GameObject
-            dialogue.StartDialogue(); // Start the dialogue sequence
-        }
-        else
-        {
-            Debug.LogWarning("Dialogue or DialogueData not set for " + transform.name);
-        }
+        Debug.Log("My name is " + transform.name);
 
-        Instantiate(polaroid, new Vector2(0, 0), Quaternion.identity, inventoryMenu.transform);
+        // adds a polaroid to your inventory and logic menu
+        Instantiate(polaroid, new Vector2(0, 0), Quaternion.identity, polaroidInventory.transform);
+        Instantiate(logicPolaroid, new Vector2(0, 0), Quaternion.identity, logicInventory.transform);
     }
 }
